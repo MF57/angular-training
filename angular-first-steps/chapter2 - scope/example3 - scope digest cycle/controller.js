86425db9e718218251d3c0de6e0188211572115b
MF57 <p-bochenek@outlook.com>
2016-06-18 23:15:52 +0200

    digest cycle

--- angular-first-steps/chapter2 - scope/example3 - scope digest cycle/controller.js
+++ angular-first-steps/chapter2 - scope/example3 - scope digest cycle/controller.js
@@ -1,9 +1,11 @@
 var app = angular.module('app', []);
 
 app.controller('DefaultCtrl', function ($scope) {
+    
     $scope.number = 1;
-
-
+    
+    //Explicit watcher object to $scope.number  (implicit watchers are created via interpolation)
+    //Uses angular.copy and angular.equals for comparing and copying value of watched object
     $scope.$watch('number', function () {
        console.log("Number changed to: " + $scope.number);
     });
@@ -14,5 +16,40 @@ app.controller('DefaultCtrl', function ($scope) {
 
     $scope.subtract = function () {
         $scope.number--;
-    }
+    };
+
+    $scope.tryToAddWithDelay = function () {
+        //changing scope object from outside angular doesn't trigger digest cycle which won't change anything in view
+        //however the change in the scope object took place so when the digest cycle will be called
+        // the change will be taken into the account
+        setTimeout(function() {
+            $scope.number++;
+            console.log('Adding with delay but it doesnt trigger digest cycle');
+        }, 2000)
+    };
+
+    $scope.tryToSubtractWithDelay = function () {
+        setTimeout(function() {
+            $scope.number--;
+            console.log('Subtracting with delay but it doesnt trigger digest cycle');
+        }, 2000)
+    };
+
+    $scope.addWithDelay = function () {
+        setTimeout(function() {
+            $scope.number++;
+            console.log('Adding with delay');
+            //manually triggering digest cycle
+            //NEVER call $digest, but apply
+            $scope.$apply();
+        }, 2000)
+    };
+
+    $scope.subtractWithDelay = function () {
+        setTimeout(function() {
+            $scope.number--;
+            console.log('Subtracting with delay');
+            $scope.$apply();
+        }, 2000)
+    };
 });
