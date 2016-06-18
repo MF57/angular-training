54e2bf6ef58946c0fbc435c0649f7a2dc5491c83
MF57 <p-bochenek@outlook.com>
2016-06-18 22:49:29 +0200

    $watch example

--- angular-first-steps/chapter2 - scope/example3 - scope digest cycle/controller.js
+++ angular-first-steps/chapter2 - scope/example3 - scope digest cycle/controller.js
@@ -1,5 +1,18 @@
 var app = angular.module('app', []);
 
 app.controller('DefaultCtrl', function ($scope) {
+    $scope.number = 1;
 
+
+    $scope.$watch('number', function () {
+       console.log("Number changed to: " + $scope.number);
+    });
+    
+    $scope.add = function () {
+        $scope.number++;
+    };
+
+    $scope.subtract = function () {
+        $scope.number--;
+    }
 });
