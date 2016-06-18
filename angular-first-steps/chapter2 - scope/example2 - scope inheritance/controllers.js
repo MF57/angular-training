8ae8a6cadcaa46e9bdceba810a1722347097ad9a
MF57 <p-bochenek@outlook.com>
2016-06-18 22:25:48 +0200

    angular scope inheritance

--- angular-first-steps/chapter2 - scope/example2 - scope inheritance/controllers.js
+++ angular-first-steps/chapter2 - scope/example2 - scope inheritance/controllers.js
@@ -4,13 +4,33 @@
 
 var app = angular.module('app', []);
 
+app.run(function ($rootScope) {
+    $rootScope.studenClass = "III C";
+});
 
-app.controller('ParentCtrl', function ($scope) {
-    $scope.student = {passed: false };
+
+app.controller('ParentCtrl', function ($scope, $rootScope) {
+    $scope.student = {passed: false};
+    $scope.grades = [
+        {subject: "Math", mark: 5.0},
+        {subject: "Science", mark: 4.0},
+        {subject: "Arts", mark: 3.5}
+    ];
+
+    //Angular inheritance example
+    var rootObject = $rootScope;
+    var inheritedObject = rootObject.$new();
+    rootObject.teacherName = 'Ian';
+    inheritedObject.teacherSurname = 'McCooper';
+
+    //will print Ian undefined
+    console.log(inheritedObject.teacherName + ' ' + inheritedObject.teacherSurname);
+    //will print Ian McCooper
+    console.log(rootObject.teacherName + ' ' + rootObject.teacherSurname);
 });
 
 app.controller('InheritanceCtrl', function ($scope) {
-    $scope.checkTest = function() {
+    $scope.checkTest = function () {
         $scope.student.passed = !$scope.student.passed;
     }
 });
